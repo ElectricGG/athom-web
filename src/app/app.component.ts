a2b72d354366f33ce3151cd2f897b73627c90e85
Jesus Cuadros <jesus_cuadros_tammara@hotmail.com>
2026-03-01 17:00:10 -0500

    feat: add global scroll animations using AOS library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `<router-outlet />`
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    AOS.init({
+      once: true,
+      offset: 50,
+      duration: 600,
+      easing: 'ease-out-cubic',
+    });
+  }
+}
